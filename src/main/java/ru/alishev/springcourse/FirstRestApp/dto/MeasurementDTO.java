1a979519b39185e745467fa9d66661f8840fdc14
nezox323423 <58356519+nezox323423@users.noreply.github.com>
2023-04-21 20:31:27 +0300

    3 commit

--- src/main/java/ru/alishev/springcourse/FirstRestApp/dto/MeasurementDTO.java
+++ src/main/java/ru/alishev/springcourse/FirstRestApp/dto/MeasurementDTO.java
@@ -1,11 +1,15 @@
 package ru.alishev.springcourse.FirstRestApp.dto;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
+@Data
 public class MeasurementDTO {
+
     @Size(min = -100, max = 100, message = "Value should be from -100 to 100 characteristics")
     @NotNull
     private double value;
@@ -19,35 +23,4 @@ public class MeasurementDTO {
     @NotNull
     private SensorDTO sensor;
 
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
-
-    public boolean isRaining() {
-        return raining;
-    }
-
-    public void setRaining(boolean raining) {
-        this.raining = raining;
-    }
-
-    public LocalDateTime getMeasurementDateTime() {
-        return measurementDateTime;
-    }
-
-    public void setMeasurementDateTime(LocalDateTime measurementDateTime) {
-        this.measurementDateTime = measurementDateTime;
-    }
-
-    public SensorDTO getSensor() {
-        return sensor;
-    }
-
-    public void setSensor(SensorDTO sensor) {
-        this.sensor = sensor;
-    }
 }
